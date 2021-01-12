910664064ad1448e081dfc117b0e680be65aa202
Yocss <yeahoor@qq.com>
2021-01-12 17:45:00 +0800

    修改了请求类

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -17,14 +17,11 @@ export default defineComponent({
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     async function getData (): Promise<any> {
       const data = await axios.post('https://lm.sihongapi.top/v3/news/news_index', {
-      // const data = await axios.get('/dy/article/FVLRS2F60521QBNK.html?clickfrom=w_yw', {
-      // const data = await axios.post('https://llm.sihongapi.top/v3/news/news_index', {
-      // const data = await axios.post('https://www.jzlm360.com/', {
         region: 360000,
         device: 'mb',
         profession: 235,
-        version: 0,
-        page: 1
+        version: 0
+        // page: 1
       })
       console.log(data)
     }
--- src/plugins/http.ts
+++ src/plugins/http.ts
@@ -1,31 +1,71 @@
-import { Axios } from './axios'
-const axios = new Axios({
-  timeout: 10000,
-  baseURL: '/',
-  jsonData: false,
-  customCode: { success: [0, 2000, 2010] },
-  formatKeys: {
-    code: 'code',
-    result: 'data',
-    message: 'info'
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Axios } from './axios/Index'
+import { Notify } from 'vant'
+interface OptionType {
+  baseURL?: string;
+  timeout?: number;
+  headers?: Record<string, any>;
+  [key: string]: any;
+}
+interface ConfigType {
+  method?: 'post' | 'get' | 'put' | 'patch' | 'delete' | 'head' | 'options';
+  option?: OptionType;
+  showNotify?: boolean;
+  [key: string]: any;
+}
+class Http extends Axios {
+  private handleError (responseData: any): void {
+    console.log(responseData)
+    // 1. 错误分级
+    // 2. 重试
+    // 3. 弹出提示
   }
-})
-class Http {
+
+  private async request (url: string, query: Record<string, any>, config: ConfigType = {}) {
+    const baseURL = process.env === 'production' ? '/api' : '/'
+    const option: ConfigType = {
+      method: 'get',
+      showNotify: false,
+      option: { baseURL, headers: {}, timeout: 10000 }
+    }
+    const result = await this.ajax(url, query, Object.assign(option, config))
+    const { data } = result
+    if (data.code !== 0) {
+      Notify({
+        type: 'danger',
+        message: data.message
+      })
+    }
+    return data
+  }
+
   /**
-   * 发请post请求
+   * 发起post请求
    * @param url 接口地址
    * @param data 请求参数
    * @param config 修改请求配置
    */
-  async post (url: string, data: JSON) {
-    const result = axios.request(url, data, {
-      method: 'post',
-      option: {
-        headers: {}
-      }
-    })
+  async post (url: string, data: Record<string, any>, config: ConfigType = {}) {
+    const option: ConfigType = { showNotify: true }
+    const result = await this.request(url, data, Object.assign(option, config, { method: 'post' }))
+    return result
+  }
+
+  /**
+   * 发起get请求
+   * @param url 接口地址
+   * @param data 请求参数
+   * @param config 请求配置
+   */
+  async get (url: string, data: Record<string, any>, config: ConfigType = {}) {
+    const option: ConfigType = { showNotify: true }
+    const result = await this.request(url, data, Object.assign(option, config, { method: 'get' }))
     return result
   }
 }
 
-export default new Http()
+export default new Http({
+  jsonData: false, // 不使用json提交
+  successCode: [0, 2000, 2010], // 与后端约定表示成功的代码
+  formatKeys: { code: 'code', result: 'data', message: 'info' } // 与后端约定的基础返回字段
+})
