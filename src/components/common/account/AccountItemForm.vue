d259dff902bc381fbb25b7b4660535dc6d563473
Axmine <yeahoor@qq.com>
2021-06-03 18:01:17 +0800

    搞定获取验证码时的倒计时 以及 何时显示发送验证码按钮

--- src/components/common/account/AccountItemForm.vue
+++ src/components/common/account/AccountItemForm.vue
@@ -3,7 +3,7 @@
     <template v-for="field in fields" :key="field">
       <!-- v-model="form[field]" -->
       <van-field
-        v-model="field.value"
+        v-model.trim="field.value"
         size="large"
         v-bind="field.attrs"
         class="account-field"
@@ -13,19 +13,21 @@
           #button
         >
           <van-button
-            disabled
+            :disabled="disabled"
             type="primary"
             round
             size="small"
-          >获取验证码</van-button>
+            @click="getCode"
+          >{{ codeText }}</van-button>
         </template>
       </van-field>
     </template>
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent, reactive, ref, watch } from 'vue'
 import { Field, Button } from 'vant'
+import { FieldType } from './data'
 export default defineComponent({
   name: 'AccountItemForm',
   components: {
@@ -37,6 +39,50 @@ export default defineComponent({
       type: Array,
       default: () => { return [] }
     }
+  },
+  setup (prop, { emit }) {
+    const codeText = ref('获取验证码')
+    const loading = ref(false)
+    const data = reactive({ timer: 0 })
+    const disabled = computed(() => {
+      const fields = prop.fields as Array<FieldType>
+      let bool = true
+      const user = fields.find(e => e.key === 'user')
+      // 需要使用到 code 的地方，并且手机号存在时，如果手机号合法，则将发送验证码置为允许
+      if (fields.some((e) => e.key === 'code') && user?.value) {
+        const rule = user.rules.rules.find(e => e.pattern)
+        if (rule) {
+          const reg = rule.pattern as RegExp
+          const v = user.value as string
+          bool = !reg.test(v)
+        }
+      }
+      return loading.value || bool
+    })
+    const setCodeText = (t: number): void => {
+      clearInterval(data.timer)
+      data.timer = setInterval(() => {
+        t--
+        if (t < 1) {
+          codeText.value = '重新发送'
+          clearInterval(data.timer)
+          loading.value = false
+        } else {
+          codeText.value = '已发送 ' + (t > 9 ? t : '0' + t)
+        }
+      }, 1000)
+    }
+    watch(loading, (v) => {
+      if (v) {
+        codeText.value = '已发送 60'
+        setCodeText(60)
+      }
+    })
+    const getCode = () => {
+      loading.value = true
+      emit('event', { action: 'getCode' })
+    }
+    return { disabled, getCode, codeText }
   }
 })
 </script>
