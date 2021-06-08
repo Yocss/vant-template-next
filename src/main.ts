e3868dd157d3243f5cf42a107225b8ac15004158
Axmine <yeahoor@qq.com>
2021-06-08 17:06:51 +0800

    增加懒加载指令

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,15 @@ import './plugins/registerServiceWorker'
 import router from './router'
 import { store, key } from './store'
 import './plugins/routerHook'
+import { Lazyload } from 'vant'
 
 // createApp(App).use(store, key).use(router).mount('#app')
 export const app = createApp(App)
 app.use(store, key)
+app.use(Lazyload, {
+  lazyComponent: true,
+  loading: 'https://sihong-cdn.oss-cn-shanghai.aliyuncs.com/img/loading.gif',
+  error: '/error.png'
+})
 app.use(router)
 app.mount('#app')
