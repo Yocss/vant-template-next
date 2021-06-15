23fa582d6a8cd47ce39e97a2d4a81cb5c7b7bee5
Axmine <yeahoor@qq.com>
2021-06-15 17:53:19 +0800

    test

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,81 +1,36 @@
 <template>
-  <div class="test-index">
-    <div class="test-player">
-      <axmine-player
-        ref="axminePlayer"
-        :src="video"
-        @event="onEvent"
-      />
+  <base-page
+    class="test-index"
+    :loading="loading"
+  >
+    <div>
+      <router-link to="/test/play">播放器</router-link>
     </div>
-    <div class="test-player-btns">
-      <van-button
-        v-for="btn in btns"
-        :key="btn.title"
-        @click="toggleVideo(btn.action)"
-      >
-        {{ btn.title }}
-      </van-button>
-    </div>
-  </div>
+  </base-page>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
-import { Button } from 'vant'
-import { EventType } from '@/common/interface'
-import AxminePlayer from '@/components/common/player/index.vue'
+import { defineComponent, onMounted, ref } from 'vue'
+import BasePage from '@/components/base/BasePage/index.vue'
 
 export default defineComponent({
-  name: 'TestIndex',
+  name: 'TextIndex',
   components: {
-    [Button.name]: Button,
-    AxminePlayer
+    BasePage
   },
   setup () {
-    // const video = ref('http://boliu.jiankao.wang/live/app.m3u8?auth_key=1623389727-0-0-4f1b50e0691b78996c92d7362e474183')
-    const video = ref('https://vod.jiankao.wang/2202c169cc614d6c87bc18577ce0cf3f/bef9cd89fcb348728dbd5a68477a110a-27ccfc3e8264433c1bd3c6ec23e0565a-sd.m3u8')
-    const axminePlayer = ref(null)
-    const btns = ref([
-      { title: '切换src', action: 'toggle' },
-      { title: '播放', action: 'play' },
-      { title: '暂停', action: 'pause' },
-      { title: '静音', action: 'muted' }
-    ])
-    const toggleVideo = (action: string) => {
-      const player: Record<string, Function> = axminePlayer.value || {}
-      switch (action) {
-        case 'toggle': {
-          const l1 = 'https://vod.jiankao.wang/366c82d7ef4247efae7da2104713ebaf/c8c454d968144b079cfe0d5c901f2b5e-74895cf715fd7e56ede8175efa4bfcc7-sd.mp4'
-          const l2 = 'https://vod.jiankao.wang/7c01465c94e449eeb2c795909d6b5eca/84b66563011d4411b801161ac54cd95a-6a3736091d286b946486bc6e0da0fdc7-sd.mp4'
-          video.value = video.value === l1 ? l2 : l1
-          // video.value = 'http://boliu.jiankao.wang/live/app.m3u8?auth_key=1623389727-0-0-4f1b50e0691b78996c92d7362e474183'
-          console.log('haha')
-          const a = player.invoke('play')
-          console.log(a)
-          break
-        }
-        case 'play':
-          player.invoke('play')
-          break
-        case 'pause':
-          player.invoke('pause')
-          break
-        case 'muted': {
-          const b = player.invoke('muted')
-          player.invoke('muted', !b)
-          break
-        }
-      }
-    }
-    const onEvent = (event: EventType) => {
-      console.log(event)
-    }
-    return {
-      axminePlayer,
-      btns,
-      video,
-      onEvent,
-      toggleVideo
-    }
+    const loading = ref(true)
+    onMounted(() => {
+      // loading.value = false
+      setTimeout(() => {
+        loading.value = false
+      }, 500)
+    })
+    return { loading }
   }
 })
 </script>
+<style lang="stylus">
+@import '~@/assets/css/common/variables.styl'
+test-index
+  padding $font
+</style>
---
+++ src/views/test/play.vue
@@ -0,0 +1,105 @@
+<template>
+  <base-page
+    class="play-index"
+    :loading="loading"
+  >
+    <div class="test-player">
+      <axmine-player
+        ref="axminePlayer"
+        :src="video"
+        @event="onEvent"
+      />
+    </div>
+    <div class="test-player-btns">
+      <van-button
+        v-for="btn in btns"
+        :key="btn.title"
+        @click="toggleVideo(btn.action)"
+      >
+        {{ btn.title }}
+      </van-button>
+    </div>
+    <h1
+      v-for="item in 15"
+      :key="item"
+    >中华人民共和国中华人民共和国</h1>
+  </base-page>
+</template>
+<script lang="ts">
+import { defineComponent, onMounted, ref } from 'vue'
+import { Button } from 'vant'
+import { useStore } from '@/store'
+import { EventType } from '@/common/interface'
+import BasePage from '@/components/base/BasePage/index.vue'
+import AxminePlayer from '@/components/common/player/index.vue'
+
+export default defineComponent({
+  name: 'TestIndex',
+  components: {
+    BasePage,
+    [Button.name]: Button,
+    AxminePlayer
+  },
+  setup () {
+    const store = useStore()
+    const loading = ref(true)
+    const video = ref('http://boliu.jiankao.wang/live/app.m3u8?auth_key=1623389727-0-0-4f1b50e0691b78996c92d7362e474183')
+    // const video = ref('https://vod.jiankao.wang/2202c169cc614d6c87bc18577ce0cf3f/bef9cd89fcb348728dbd5a68477a110a-27ccfc3e8264433c1bd3c6ec23e0565a-sd.m3u8')
+    // const video = ref('')
+    const axminePlayer = ref(null)
+    const btns = ref([
+      { title: '切换src', action: 'toggle' },
+      { title: '播放', action: 'play' },
+      { title: '暂停', action: 'pause' },
+      { title: '静音', action: 'muted' },
+      { title: '登录', action: 'open' }
+    ])
+    const toggleVideo = (action: string) => {
+      const player: Record<string, Function> = axminePlayer.value || {}
+      switch (action) {
+        case 'toggle': {
+          const l1 = 'https://vod.jiankao.wang/366c82d7ef4247efae7da2104713ebaf/c8c454d968144b079cfe0d5c901f2b5e-74895cf715fd7e56ede8175efa4bfcc7-sd.mp4'
+          // const l2 = 'https://vod.jiankao.wang/7c01465c94e449eeb2c795909d6b5eca/84b66563011d4411b801161ac54cd95a-6a3736091d286b946486bc6e0da0fdc7-sd.mp4'
+          const l2 = 'https://vod.jiankao.wang/2202c169cc614d6c87bc18577ce0cf3f/bef9cd89fcb348728dbd5a68477a110a-27ccfc3e8264433c1bd3c6ec23e0565a-sd.m3u8'
+          // 获取当前的播放地址
+          let src = player.invoke('src')
+          src = src === l2 ? l1 : l2
+          player.invoke('src', src)
+          break
+        }
+        case 'play': {
+          player.invoke('play')
+          break
+        }
+        case 'pause':
+          player.invoke('pause')
+          break
+        case 'muted': {
+          const b = player.invoke('muted')
+          player.invoke('muted', !b)
+          break
+        }
+        case 'open':
+          store.dispatch('SetStore', { account: { visible: true } })
+          break
+      }
+    }
+    const onEvent = (event: EventType) => {
+      if (event.action !== 'timeupdate' && event.action !== 'progress') {
+        console.log(event)
+      }
+    }
+    onMounted(() => {
+      loading.value = false
+    })
+    return {
+      loading,
+      axminePlayer,
+      btns,
+      video,
+      onEvent,
+      toggleVideo
+    }
+  }
+})
+</script>
