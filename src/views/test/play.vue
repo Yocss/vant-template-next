568d6e0b9ed8cc8847b41892cad87956aab84dbc
Axmine <yeahoor@qq.com>
2021-06-16 17:55:43 +0800

    调整播放器测试页

--- src/views/test/play.vue
+++ src/views/test/play.vue
@@ -3,70 +3,66 @@
     class="play-index"
     :loading="loading"
   >
-    <div class="test-player">
+    <div class="player-box">
       <axmine-player
         ref="axminePlayer"
         :src="video"
         @event="onEvent"
       />
     </div>
-    <div class="test-player-btns">
+    <div class="play-index-btns flex-center">
       <van-button
         v-for="btn in btns"
         :key="btn.title"
         @click="toggleVideo(btn.action)"
+        type="primary"
+        size="small"
+        round
       >
         {{ btn.title }}
       </van-button>
     </div>
-    <h1
-      v-for="item in 15"
-      :key="item"
-    >中华人民共和国中华人民共和国</h1>
+    <div class="play-index-info">
+      <p>1. 支持 m3u8 及 mp4 格式</p>
+      <p>2. 支持在线 m3u8 直播流</p>
+      <p>3. 支持 m3u8 与 mp4 随意切换</p>
+      <p>4. 第一次播放时必须手动触发</p>
+      <p>5. iOS 微信端及自带浏览器完全兼容</p>
+      <p>6. Android 兼容微信浏览器，其他不保证</p>
+      <p>7. 播放器状态通过监听 event 事件实时反馈</p>
+      <p>8. 通过ref获取实例调用 invoke 方法进行控制</p>
+    </div>
   </base-page>
 </template>
 <script lang="ts">
 import { defineComponent, onMounted, ref } from 'vue'
 import { Button } from 'vant'
-import { useStore } from '@/store'
 import { EventType } from '@/common/interface'
-import BasePage from '@/components/base/BasePage/index.vue'
 import AxminePlayer from '@/components/common/player/index.vue'
 
 export default defineComponent({
   name: 'TestIndex',
   components: {
-    BasePage,
     [Button.name]: Button,
     AxminePlayer
   },
   setup () {
-    const store = useStore()
     const loading = ref(true)
-    const video = ref('http://boliu.jiankao.wang/live/app.m3u8?auth_key=1623389727-0-0-4f1b50e0691b78996c92d7362e474183')
-    // const video = ref('https://vod.jiankao.wang/2202c169cc614d6c87bc18577ce0cf3f/bef9cd89fcb348728dbd5a68477a110a-27ccfc3e8264433c1bd3c6ec23e0565a-sd.m3u8')
-    // const video = ref('')
+    const video = ref('/movies/av.mp4')
     const axminePlayer = ref(null)
     const btns = ref([
-      { title: '切换src', action: 'toggle' },
+      // { title: '切换src', action: 'toggle' },
       { title: '播放', action: 'play' },
       { title: '暂停', action: 'pause' },
-      { title: '静音', action: 'muted' },
-      { title: '登录', action: 'open' }
+      { title: '静音', action: 'muted' }
     ])
     const toggleVideo = (action: string) => {
       const player: Record<string, Function> = axminePlayer.value || {}
       switch (action) {
-        case 'toggle': {
-          const l1 = 'https://vod.jiankao.wang/366c82d7ef4247efae7da2104713ebaf/c8c454d968144b079cfe0d5c901f2b5e-74895cf715fd7e56ede8175efa4bfcc7-sd.mp4'
-          // const l2 = 'https://vod.jiankao.wang/7c01465c94e449eeb2c795909d6b5eca/84b66563011d4411b801161ac54cd95a-6a3736091d286b946486bc6e0da0fdc7-sd.mp4'
-          const l2 = 'https://vod.jiankao.wang/2202c169cc614d6c87bc18577ce0cf3f/bef9cd89fcb348728dbd5a68477a110a-27ccfc3e8264433c1bd3c6ec23e0565a-sd.m3u8'
-          // 获取当前的播放地址
-          let src = player.invoke('src')
-          src = src === l2 ? l1 : l2
-          player.invoke('src', src)
-          break
-        }
+        // case 'toggle': {
+        //   player.invoke('src', '/test.mp4')
+        //   break
+        // }
         case 'play': {
           player.invoke('play')
           break
@@ -79,9 +75,6 @@ export default defineComponent({
           player.invoke('muted', !b)
           break
         }
-        case 'open':
-          store.dispatch('SetStore', { account: { visible: true } })
-          break
       }
     }
     const onEvent = (event: EventType) => {
@@ -90,7 +83,10 @@ export default defineComponent({
       }
     }
     onMounted(() => {
-      loading.value = false
+      const t = setTimeout(() => {
+        loading.value = false
+        clearTimeout(t)
+      }, 500)
     })
     return {
       loading,
@@ -103,3 +99,18 @@ export default defineComponent({
   }
 })
 </script>
+<style lang="stylus">
+.play-index
+  .player-box
+    width 100%
+    height 420px
+    background-color #000
+  &-btns
+    padding 50px 0
+    button
+      margin 0 15px
+  &-info
+    padding 32px
+    p
+      margin-bottom 16px
+</style>
