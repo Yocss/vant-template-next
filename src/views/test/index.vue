d45e3c85371357e022f310e54c72d40dd5267253
Axmine <yeahoor@qq.com>
2021-06-16 17:56:55 +0800

    改美观些，之前太丑了

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -1,36 +1,63 @@
 <template>
   <base-page
+    :head="false"
+    :loading="false"
     class="test-index"
-    :loading="loading"
   >
-    <div>
-      <router-link to="/test/play">播放器</router-link>
+    <div class="test-index-login flex-column-center">
+      <van-button
+        @click="open('login')"
+        type="primary"
+        round
+      >打开登录框</van-button>
+      <br>
+      <van-button
+        type="success"
+        round
+        @click="open('player')"
+      >
+        视频播放器
+      </van-button>
     </div>
   </base-page>
 </template>
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue'
-import BasePage from '@/components/base/BasePage/index.vue'
+import { defineComponent } from 'vue'
+import { Button } from 'vant'
+
+import { useStore } from '@/store'
+import { useRouter } from 'vue-router'
 
 export default defineComponent({
   name: 'TextIndex',
   components: {
-    BasePage
+    [Button.name]: Button
   },
   setup () {
-    const loading = ref(true)
-    onMounted(() => {
-      // loading.value = false
-      setTimeout(() => {
-        loading.value = false
-      }, 500)
-    })
-    return { loading }
+    const store = useStore()
+    const router = useRouter()
+
+    const open = (action: string) => {
+      switch (action) {
+        case 'login': {
+          store.dispatch('SetStore', { account: { visible: true } })
+          break
+        }
+        case 'player': {
+          router.push('/test/play')
+          break
+        }
+      }
+    }
+    return { open }
   }
 })
 </script>
 <style lang="stylus">
 @import '~@/assets/css/common/variables.styl'
-test-index
-  padding $font
+.test-index
+  &-login
+    padding ($font * 4)
+    width 100%
+    height 100%
 </style>
