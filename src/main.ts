784cc97d3e62f9c3ba2a420699e2ae0ac0eeb692
Axmine <yeahoor@qq.com>
2021-06-16 17:51:32 +0800

    将base-page定义为全局组件

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,12 @@ import router from './router'
 import { store, key } from './store'
 import './plugins/routerHook'
 import { Lazyload } from 'vant'
+// 全局base-page组件
+import BasePage from '@/components/base/BasePage/index.vue'
 
 // createApp(App).use(store, key).use(router).mount('#app')
 export const app = createApp(App)
+app.component('base-page', BasePage)
 app.use(store, key)
 app.use(Lazyload, {
   lazyComponent: true,
